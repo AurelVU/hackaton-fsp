dd32cb197c8c183d96cb9a76dae97c36277a07fd
Ушаков Владимир <eau120676@gmail.com>
2023-04-28 06:53:21 +0300

    updated

--- app/schema/user.py
+++ app/schema/user.py
@@ -1,4 +1,4 @@
-from flask_restx import fields
+from marshmallow import fields
 from marshmallow.fields import Nested
 from marshmallow_enum import EnumField
 from marshmallow_sqlalchemy import auto_field
@@ -14,11 +14,17 @@ class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = User
         load_instance = True
-        include_fk = True
+        include_fk = False
         include_relationships = True
 
     id = auto_field(dump_only=True)
+    name = auto_field()
+    username = auto_field()
+    email = auto_field()
+    rating = auto_field()
     hashed_password = auto_field(load_only=True)
+    is_activated = auto_field()
     type = EnumField(Type, by_value=True)
     city = ma.Nested(CitySchema(only=("name",)))
-    city_name = fields.String(attribute="city.name")
+    city_id = fields.String(attribute="city.name")
+    team_id = auto_field()  # Добавьте поле внешнего ключа, которое вы хотите сериализовать
